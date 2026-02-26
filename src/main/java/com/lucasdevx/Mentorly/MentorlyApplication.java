51f1e7599d6248e6bc6bc679101e6a03826e4f7b
Lucas Lima <lucaslimadevx@gmail.com>
2026-02-25 23:55:44 -0300

    Configurated connection with mysql database and created package named model with User class

---
+++ src/main/java/com/lucasdevx/Mentorly/MentorlyApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdevx.Mentorly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorlyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lucasdevx/Mentorly/model/User.java
@@ -0,0 +1,49 @@
+package com.lucasdevx.Mentorly.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_users")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_fullName", nullable = false, length = 255)
+	private String fullName;
+	
+	@Column(name = "user_email", nullable = false, length = 255)
+	private String email;
+	
+	@Column(name = "user_password", nullable = false, length = 255)
+	private String password;
+	
+	@Column(name = "user_active")
+	private boolean active;
+	
+	@Column(nullable = false)
+	private Date createdAt;
+	
+	@Column(nullable = false)
+	private Date updatedAt;
+
+}
