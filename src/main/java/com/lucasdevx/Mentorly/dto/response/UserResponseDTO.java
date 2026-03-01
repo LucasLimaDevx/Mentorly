8d6bf34e35f7cace4e9f7d666702a23153efb2e6
Lucas Lima <lucaslimadevx@gmail.com>
2026-03-01 00:02:30 -0300

    Remove empty spaces from UserResponseDTO

--- src/main/java/com/lucasdevx/Mentorly/dto/response/UserResponseDTO.java
+++ src/main/java/com/lucasdevx/Mentorly/dto/response/UserResponseDTO.java
@@ -3,8 +3,6 @@ package com.lucasdevx.Mentorly.dto.response;
 import java.io.Serializable;
 import java.util.Date;
 
-import org.springframework.hateoas.RepresentationModel;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lucasdevx.Mentorly.model.User;
 
