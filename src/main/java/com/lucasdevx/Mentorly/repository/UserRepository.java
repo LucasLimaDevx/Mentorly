3f020c1a9a370d1c8fa704016d828677ac1406af
Lucas Lima <lucaslimadevx@gmail.com>
2026-03-26 23:03:11 -0300

    Defined a abstract method findUserByEmail on UserRepository

--- src/main/java/com/lucasdevx/Mentorly/repository/UserRepository.java
+++ src/main/java/com/lucasdevx/Mentorly/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.lucasdevx.Mentorly.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.lucasdevx.Mentorly.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	Optional<UserDetails> findUserByEmail(String email);
 }
