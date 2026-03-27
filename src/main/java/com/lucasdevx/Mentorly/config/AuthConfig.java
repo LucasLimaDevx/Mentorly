62ab3b8310ecc6e11147dde77a1026c304e3b39e
Lucas Lima <lucaslimadevx@gmail.com>
2026-03-26 23:23:21 -0300

    Created AuthConfig class and implemented loadUserByUsername method from UserDetailsService interface

---
+++ src/main/java/com/lucasdevx/Mentorly/config/AuthConfig.java
@@ -0,0 +1,26 @@
+package com.lucasdevx.Mentorly.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.lucasdevx.Mentorly.repository.UserRepository;
+
+@Service
+public class AuthConfig implements UserDetailsService {
+	
+	private final UserRepository userRepository;
+
+	public AuthConfig(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return userRepository.findUserByEmail(username).get();
+	}
+	
+	
+}
