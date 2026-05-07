e7701682d0ba8c1b4beb4834a16997d829be7ece
Lucas Lima <lucaslimadevx@gmail.com>
2026-05-06 21:45:58 -0300

    Created findByIdWithUserRole and findByIdWithAdminRole method's tests

--- src/test/java/com/lucasdevx/Mentorly/service/UserServiceTest.java
+++ src/test/java/com/lucasdevx/Mentorly/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -63,12 +64,6 @@ class UserServiceTest {
 		User user = input.mockEntity(2);
 		User userPersisted = user;
 		UserResponseDTO response = input.mockResponseDTO(2);
-		/*
-		when(userMapper.converterToEntity(any(UserRequestDTO.class))).thenReturn(user);
-		when(userRepository.save(any(User.class))).thenReturn(userPersisted);
-		when(userMapper.converterToDto(any(User.class))).thenReturn(response);
-	
-		*/
 		
 		when(userMapper.converterToEntity(request)).thenReturn(user);
 		when(userRepository.save(user)).thenReturn(userPersisted);
@@ -77,7 +72,7 @@ class UserServiceTest {
 		
 		assertNotNull(result);
 		assertNotNull(result.getContent().id());
-		
+		assertEquals(2L, result.getContent().id());
 		assertEquals("First Name Test2", result.getContent().firstName());
 		assertEquals("Last Name Test2", result.getContent().lastName());
 		assertEquals("emailtest2@gmail.com", result.getContent().email());
@@ -101,23 +96,60 @@ class UserServiceTest {
 		var result = userService.create(request, "admin");
 		
 		assertNotNull(result);
-		
-		assertNotNull(result);
 		assertNotNull(result.getContent().id());
-		
+		assertEquals(1L, result.getContent().id());
 		assertEquals("First Name Test1", result.getContent().firstName());
 		assertEquals("Last Name Test1", result.getContent().lastName());
 		assertEquals("emailtest1@gmail.com", result.getContent().email());
 		assertEquals("ADMIN", result.getContent().role());
 		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().createdAt()));
 		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().uptedAt()));
-		
 	
 		
 	}
 
 	@Test
-	void findById() {
+	void findByIdWithUserRole() throws ParseException {
+		User userPersisted = input.mockEntity(2);
+		UserResponseDTO response = input.mockResponseDTO(2);
+		
+		when(userRepository.findById(2L)).thenReturn(Optional.of(userPersisted));
+		when(userMapper.converterToDto(userPersisted)).thenReturn(response);
+		
+		var result = userService.findById(2L);
+		
+		assertNotNull(result);
+		assertNotNull(result.getContent().id());
+		assertEquals(2L, result.getContent().id());
+		assertEquals("First Name Test2", result.getContent().firstName());
+		assertEquals("Last Name Test2", result.getContent().lastName());
+		assertEquals("emailtest2@gmail.com", result.getContent().email());
+		assertEquals("USER", result.getContent().role());
+		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().createdAt()));
+		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().uptedAt()));
+		
+		
+	}
+	
+	@Test
+	void findByIdWithAdminRole() throws ParseException {
+		User userPersisted = input.mockEntity(1);
+		UserResponseDTO response = input.mockResponseDTO(1);
+		
+		when(userRepository.findById(1L)).thenReturn(Optional.of(userPersisted));
+		when(userMapper.converterToDto(userPersisted)).thenReturn(response);
+		
+		var result = userService.findById(1L);
+		
+		assertNotNull(result);
+		assertNotNull(result.getContent().id());
+		assertEquals(1L, result.getContent().id());
+		assertEquals("First Name Test1", result.getContent().firstName());
+		assertEquals("Last Name Test1", result.getContent().lastName());
+		assertEquals("emailtest1@gmail.com", result.getContent().email());
+		assertEquals("ADMIN", result.getContent().role());
+		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().createdAt()));
+		assertEquals("12/07/2026 10:00", formatter.format(result.getContent().uptedAt()));
 	}
 
 	@Test
